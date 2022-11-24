41cf21eb40a47d27cd4067dac017a4ef7ac26777
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-24 11:06:43 +0300

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,18 @@ class MoviesController < ApplicationController
   def show
     movie = Movie.find(params[:id])
     render json: movie
+   #no need for this since we have genrarated a serializer and updated it with the following attributes
+    #render json: movie.to_json(only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
+  end
+
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movie = Movie.all
+    render json: movie, each_serializer: MovieSummarySerializer
   end
 
   private
